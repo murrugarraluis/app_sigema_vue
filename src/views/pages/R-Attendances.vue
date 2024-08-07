4129f852cc5295ea20c7c7d67fbef0bc15a76505
Luis Murrugarra <luis@combativa.com>
2024-08-07 00:40:41 -0500

    :sparkles: feat: update Report pages

--- src/views/pages/R-Attendances.vue
+++ src/views/pages/R-Attendances.vue
@@ -60,7 +60,8 @@ export default {
             documentTypeItems: null,
             loadingEmployees: true,
             isView: false,
-            urlPDF: null
+            urlPDF: null,
+            loadGenerate: false
         };
     },
     /*components: {
@@ -77,6 +78,7 @@ export default {
     },
     methods: {
         generate() {
+            this.loadGenerate = true;
             let payload = {
                 start_date: moment(this.start_date).format('YYYY-MM-DD'),
                 end_date: moment(this.end_date).format('YYYY-MM-DD'),
@@ -86,7 +88,8 @@ export default {
             };
             this.attendanceService.report(payload).then((data) => {
                 if (data.path) {
-                    this.urlPDF = `${process.env.VUE_APP_API_HOST}/storage/${data.path}`;
+                    this.loadGenerate = false;
+                    this.urlPDF = `${import.meta.env.VITE_APP_API_HOST}/storage/${data.path}`;
                     window.open(this.urlPDF);
                 }
             });
@@ -100,77 +103,73 @@ export default {
 </script>
 
 <template>
-    <Toast />
-    <div class="grid">
-        <div class="col-12">
-            <div class="card p-fluid">
-                <div class="flex flex-column align-items-center">
-                    <h3 class="text-900 font-medium">{{ $t('attendances_report') }}</h3>
+    <div class="card p-fluid h-full flex flex-col gap-4">
+        <h3 class="text-900 font-bold">{{ $t('attendances_report') }}</h3>
+        <div class="flex flex-col gap-4">
+            <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
+                <div>
+                    <label for="calendar1" class="block font-bold mb-3">{{ $t('from_the') }}</label>
+                    <Calendar id="calendar1" :showIcon="true" :showButtonBar="false" v-model="start_date" dateFormat="yy-mm-dd" fluid></Calendar>
+                </div>
+                <div>
+                    <label for="calendar2" class="block font-bold mb-3"> {{ $t('until_the') }}</label>
+                    <Calendar id="calendar2" :showIcon="true" :showButtonBar="false" v-model="end_date" dateFormat="yy-mm-dd" fluid></Calendar>
                 </div>
             </div>
-        </div>
-        <div class="col-12 md:col-12">
-            <div class="card p-fluid h-full">
-                <h5>{{ $t('options') }}</h5>
-                <div class="fiel grid">
-                    <div class="field col-12 sm:col-6">
-                        <label>{{ $t('from_the') }}</label>
-                        <Calendar :showIcon="true" :showButtonBar="false" v-model="start_date" dateFormat="yy-mm-dd"></Calendar>
-                    </div>
-                    <div class="field col-12 sm:col-6">
-                        <label> {{ $t('until_the') }}</label>
-                        <Calendar :showIcon="true" :showButtonBar="false" v-model="end_date" dateFormat="yy-mm-dd"></Calendar>
-                    </div>
-                </div>
-
+            <div class="grid grid-cols-1 sm:grid-cols-3">
                 <div class="field">
-                    <label>
+                    <label class="block font-bold mb-3">
                         <b>{{ $t('report_type') }}</b>
                     </label>
-                    <div class="field-radiobutton">
-                        <RadioButton v-model="type" inputId="order1" name="Attendances" value="attended" />
-                        <label for="city1">{{ $t('r_attendances') }}</label>
-                    </div>
-                    <div class="field-radiobutton">
-                        <RadioButton v-model="type" inputId="order1" name="Absences" value="missed" />
-                        <label for="city1">{{ $t('absences') }}</label>
-                    </div>
-                </div>
-                <div class="field" style="border-top: 1px solid silver"></div>
-                <div class="field">
-                    <h6>{{ $t('order_by') }}</h6>
-                    <div class="field-radiobutton">
-                        <RadioButton v-model="order" inputId="order2" name="Name" value="name" />
-                        <label for="city1">{{ $t('name') }}</label>
-                    </div>
-                    <div class="field-radiobutton">
-                        <RadioButton v-model="order" inputId="order2" name="Last name" value="lastname" />
-                        <label for="city1">{{ $t('last_name') }}</label>
-                    </div>
-                    <div class="field-radiobutton">
-                        <RadioButton v-model="order" inputId="order1" name="N° of maintenances" value="attendances" :disabled="type == 'missed'" />
-                        <label for="city1">{{ $t('n_attendances') }}</label>
-                    </div>
-                    <div class="field-radiobutton">
-                        <RadioButton v-model="order" inputId="order1" name="N° of absences" value="absences" />
-                        <label for="city1">{{ $t('n_absences') }}</label>
+                    <div class="flex flex-col gap-2">
+                        <div class="flex justify-start items-center gap-2">
+                            <RadioButton v-model="type" inputId="Attendances" name="Attendances" value="attended" />
+                            <label for="Attendances">{{ $t('r_attendances') }}</label>
+                        </div>
+                        <div class="flex justify-start items-center gap-2">
+                            <RadioButton v-model="type" inputId="Absences" name="Absences" value="missed" />
+                            <label for="Absences">{{ $t('absences') }}</label>
+                        </div>
                     </div>
                 </div>
                 <div class="field">
-                    <h6>{{ $t('scale') }}</h6>
+                    <h6 class="block font-bold mb-3">{{ $t('order_by') }}</h6>
+                    <div class="flex flex-col gap-2">
+                        <div class="flex justify-start items-center gap-2">
+                            <RadioButton v-model="order" inputId="name" name="Name" value="name" />
+                            <label for="name">{{ $t('name') }}</label>
+                        </div>
+                        <div class="flex justify-start items-center gap-2">
+                            <RadioButton v-model="order" inputId="lastname" name="Last name" value="lastname" />
+                            <label for="lastname">{{ $t('last_name') }}</label>
+                        </div>
+                        <div class="flex justify-start items-center gap-2">
+                            <RadioButton v-model="order" inputId="nroAttendances" name="N° of maintenances" value="attendances" :disabled="type == 'missed'" />
+                            <label for="nroAttendances">{{ $t('n_attendances') }}</label>
+                        </div>
+                        <div class="flex justify-start items-center gap-2">
+                            <RadioButton v-model="order" inputId="nroAbsences" name="N° of absences" value="absences" />
+                            <label for="nroAbsences">{{ $t('n_absences') }}</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="field">
+                    <h6 class="block font-bold mb-3">{{ $t('scale') }}</h6>
 
-                    <div class="field-radiobutton">
-                        <RadioButton v-model="order_by" inputId="order1" name="N° Serie" value="asc" />
-                        <label for="city1"><i class="pi pi-sort-amount-up text-green-500 text-xl"></i>{{ $t('upward') }}</label>
-                    </div>
-                    <div class="field-radiobutton">
-                        <RadioButton v-model="order_by" inputId="order2" name="Machine" value="desc" />
-                        <label for="city1"> <i class="pi pi-sort-amount-down text-green-500 text-xl"></i>{{ $t('downward') }}</label>
+                    <div class="flex flex-col gap-2">
+                        <div class="flex justify-start items-center gap-2">
+                            <RadioButton v-model="order_by" inputId="Serie" name="N° Serie" value="asc" />
+                            <label for="Serie"><i class="pi pi-sort-amount-up text-green-500 text-xl"></i>{{ $t('upward') }}</label>
+                        </div>
+                        <div class="flex justify-start items-center gap-2">
+                            <RadioButton v-model="order_by" inputId="Machine" name="Machine" value="desc" />
+                            <label for="Machine"> <i class="pi pi-sort-amount-down text-green-500 text-xl"></i>{{ $t('downward') }}</label>
+                        </div>
                     </div>
                 </div>
-                <div class="field">
-                    <Button :label="$t('generate_report')" icon="pi pi-file-pdf" class="p-button-outlined p-button-danger" @click="generate" />
-                </div>
+            </div>
+            <div class="flex justify-center mt-6">
+                <Button :label="$t('generate_report')" icon="pi pi-file-pdf" class="p-button-outlined p-button-danger" @click="generate" :loading="loadGenerate" />
             </div>
         </div>
     </div>
